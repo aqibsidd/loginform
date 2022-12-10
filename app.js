435e7829b7a39b0b7fbc472757115088af925c6d
Aqib Siddiqui <aqibsid007007@gmail.com>
2022-12-11 02:51:53 +0530

    add more
    data

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const {check, validationResult} = require('express-validator');
 const app = express();
-const port = process.env.PORT || 9000;
+const port = process.env.PORT || 9500;
 
 require('./db/conn');
 const Register = require('./models/redister');
